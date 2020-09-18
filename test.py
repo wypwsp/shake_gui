e7c77e9f79956359c38e3208a9306f150b29a296
WANG Qi <wangqi1451069@163.com>
2020-09-18 14:41:34 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# with open('sh.acc', 'r') as f:
+#     acc_1 = np.array(f.readlines())
+#     acc = []
+#     num_per_line = 0
+#     for i in acc_1:
+#         i = i.strip()
+#         if len(i):
+#             i = i.split()
+#             num_per_line = len(i)
+#             break
+# print(num_per_line)
+
+    # print(acc)
+    # acc_array = np.array(acc, dtype=int)
+    # print(np.shape(acc_array))
+
+
+
+
+
+
